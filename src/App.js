7fbcf3c29d401e6e606b06b96a6ca27a619455fd
Sumit Sudama Pandey <spandey247789@gmail.com>
2021-08-21 21:59:33 +0530

    Add components to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/header/Header";
 
 function App() {
+  // const [notes, setNotes] = useState;
+
+  // function addNote(note) {
+  //   setNotes(prevNotes => {
+  //     return [...prevNotes, note];
+  //   });
+  // }
+
+  // function deleteNote(id) {
+  //   setNotes(prevNotes => {
+  //     return prevNotes.filter((item, index) => {
+  //       return index != id;
+  //     });
+  //   });
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__heading">Google Keep</h1>
+    </header>
+  );
+}
+
+export default Header;
